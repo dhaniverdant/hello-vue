783d79cf0ec85ff6d864c713f96386a405fe1c27
dhani <dhaniverdant23@gmail.com>
2021-01-25 13:49:43 +0700

    description

---
+++ src/components/Description.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="desc-text">abcd</div>
+</template>
+
+<script>
+export default {
+  name: 'Description'
+}
+</script>
+
+<style lang="scss" scoped>
+  $color: blue;
+  .desc-text {
+    color: $color;
+    font-size: 20px;
+  }
+</style>
